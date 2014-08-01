9389eaf38e5eb3ce881dbf2445b79949042ac7d8
dv <wdv4758h@gmail.com>
2014-08-01 15:17:26 +0800

    add patch type detect and main function

--- toypatch.py
+++ toypatch.py
@@ -3,6 +3,7 @@
 
 import argparse
 import sh
+from toydiff import get_filetype, homedir_replace
 
 
 def get_parser():
@@ -12,6 +13,19 @@ def get_parser():
     add_arg('patch', metavar='patch', type=str, nargs=1)
     add_arg('output_file', metavar='new_file', type=str, nargs='?', default='')
     return parser
+
+
+def get_patchtype(filepath):
+    mime = get_filetype(filepath)
+    mime_pre = mime.partition(';')[0]
+    if mime_pre == 'text/plain':
+        head = sh.head('-n1', filepath).strip()
+        if head == 'xxd_diff':
+            return 'xxd_diff'
+        else:
+            return 'patch'
+    elif mime.strip().endswith('=binary'):
+        return 'binary'
 
 
 def bspatch(old, new, patch):
@@ -41,3 +55,28 @@ def xxd_patch(old, new, patch):
         sh.xxd('-r', '-p', tmp_old, _out=old)
 
     sh.rm('-f', tmp_old)
+
+
+def main():
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    old_file = args['old_file'][0]
+    patch = args['patch'][0]
+    output_file = args['output_file']
+
+    old_file = homedir_replace(old_file)
+    patch = homedir_replace(patch)
+    output_file = homedir_replace(output_file)
+
+    patch_type = get_patchtype(patch)
+
+    if patch_type == 'binary':
+        bspatch(old_file, output_file, patch)
+    elif patch_type == 'xxd_diff':
+        xxd_patch(old_file, output_file, patch)
+    else:
+        patch(old_file, output_file, patch)
+
+
+if __name__ == '__main__':
+    main()
